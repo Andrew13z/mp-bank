f4a309ba0289b421cfaf3b3742124422a73c6137
Andrii Krokhta <nowayoutta13@gmai.com>
2021-11-25 10:13:32 +0200

    18. Add a new default method for Service interface and 19. Add a new static method for Service interface

--- jmp-service-api/src/main/java/com/example/service/api/Service.java
+++ jmp-service-api/src/main/java/com/example/service/api/Service.java
@@ -4,6 +4,8 @@ import com.example.dto.BankCard;
 import com.example.dto.Subscription;
 import com.example.dto.User;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,4 +20,21 @@ public interface Service {
 	List<User> getAllUsers();
 
 	void addUser(User user);
+
+	default double getAverageUserAge() {
+		return getAllUsers().stream()
+				.mapToLong(Service::getAge)
+				.average()
+				.orElse(0);
+	}
+
+	static boolean isPayableUser(User user){
+		return Double.compare(getAge(user), 18) >= 0;
+	}
+
+	private static long getAge(User user) {
+		return ChronoUnit.YEARS.between(user.getBirthday(), LocalDate.now());
+	}
+
+
 }
